d023f496d28a80ac9540b40291ca6e2ed7b0b773
18ce132-Yash-Vaghani <18ce132@charusat.edu.in>
2020-10-13 12:06:35 +0530

    first commit

--- ach/android/app/src/main/kotlin/com/example/ach/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ach
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ poster_ach/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+Widget build(BuildContext context) {
+///Set color status bar
+ SystemChrome.setEnabledSystemUIOverlays([]);
+   return Scaffold(
+   backgroundColor: Colors.white,
+   body: Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        
+        Image.asset(
+          'images/logo2.png',
+  
+          alignment: Alignment.center,
+          width: 200.0,
+          height: 200.0,
+          fit: BoxFit.contain,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          'Achievement Manager',
+       
+          style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold,
+              color: Color(0xFF174D73),
+              height: 2.5,
+            fontFamily: 'Helvetica'
+            
+
+          ),
+
+        ),
+        
+        Text(
+          '"Save your achievements here...."',
+          
+          style: TextStyle(fontSize: 18,fontStyle: FontStyle.italic,
+              color: Color(0xFF174D73),
+
+            fontFamily: 'Helvetica'
+
+            
+          ), 
+        
+          ),
+      ],
+     ),
+    ),
+  );
+}
+}
